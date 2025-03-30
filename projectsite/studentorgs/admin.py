764d33be2bc3ab01730f53e964f5374823126d10
Gino Gabriel <lurcabea22@gmail.com>
2025-03-30 18:58:49 +0800

    update the code

--- projectsite/studentorgs/admin.py
+++ projectsite/studentorgs/admin.py
@@ -1,38 +1,26 @@
 from django.contrib import admin
+
 from .models import College, Program, Organization, Student, OrgMember
 
-# Customize the admin display for the College model
-class CollegeAdmin(admin.ModelAdmin):
-    list_display = ('college_name', 'created_at', 'updated_at')
-    search_fields = ('college_name',)
+admin.site.register(College)
+admin.site.register(Program)
+admin.site.register(Organization)
 
-# Customize the admin display for the Program model
-class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('prog_name', 'college', 'created_at', 'updated_at')
-    list_filter = ('college',)
-    search_fields = ('prog_name',)
-
-# Customize the admin display for the Organization model
-class OrganizationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'college', 'created_at', 'updated_at')
-    list_filter = ('college',)
-    search_fields = ('name', 'description')
-
-# Customize the admin display for the Student model
+@admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('student_id', 'lastname', 'firstname', 'program', 'created_at', 'updated_at')
-    list_filter = ('program',)
-    search_fields = ('lastname', 'firstname', 'student_id')
+    list_display = ("student_id", "lastname",
+                    "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
 
-# Customize the admin display for the OrgMember model
+@admin.register(OrgMember)
 class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ('student', 'organization', 'date_joined', 'created_at', 'updated_at')
-    list_filter = ('organization', 'date_joined')
-    search_fields = ('student__lastname', 'student__firstname', 'organization__name')
-
-# Register the models with the admin site
-admin.site.register(College, CollegeAdmin)
-admin.site.register(Program, ProgramAdmin)
-admin.site.register(Organization, OrganizationAdmin)
-admin.site.register(Student, StudentAdmin)
-admin.site.register(OrgMember, OrgMemberAdmin)
+    list_display = ("student", "get_member_program", "organization",
+                    "date_joined",)
+    search_fields = ("student__lastname", "student__firstname",)
+    
+    def get_member_program(self, obj):
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None
