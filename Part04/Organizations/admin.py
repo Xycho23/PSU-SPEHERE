d55e22055e4cc82ccc1d5a9a6e989f57696a68bb
Correa <161830862+hushmind@users.noreply.github.com>
2025-04-02 22:32:23 +0800

    Update admin.py

--- Part04/Organizations/admin.py
+++ Part04/Organizations/admin.py
@@ -1,1 +1,6 @@
+from django.contrib import admin
+from .models import Organization
 
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at') 
